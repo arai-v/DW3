6af6a94f8ede2284a5832aab96f3334dda50e4e2
Vinicius Arai <kansaiarai@gmail.com>
2024-12-01 22:29:01 -0300

    PULL

---
+++ API/src/components/Footer/index.js
@@ -0,0 +1,26 @@
+import styles from "@/components/Footer/Footer.module.css";
+
+const Footer = () => {
+  return (
+    <>
+      <div className={styles.footerContent}>
+        <div className={styles.footerLeft}>
+          <ul className={styles.footerItems}>
+            <li>Português (Brasil)</li>
+            <li>English (US)</li>
+            <li>Español</li>
+            <li>Français (France)</li>
+            <li>Italiano</li>
+          </ul>
+        </div>
+        <div className={styles.footerRight}>
+          <ul>
+            <li>The Games JR &copy; 2024 <strong>Vinicius Arai</strong></li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
